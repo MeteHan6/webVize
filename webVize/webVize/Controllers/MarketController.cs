f94a1b574d9cf3773e7f78acd873917e55e49f63
MeteHan6 <63948931+MeteHan6@users.noreply.github.com>
2020-12-07 20:45:16 +0300

    webVize

---
+++ webVize/webVize/Controllers/MarketController.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace webVize.Controllers
+{
+    public class MarketController : Controller
+    {
+        // GET: Market
+        public ActionResult Liste()
+        {
+            return View(Models.Market.MarketListesi);
+        }
+
+        public ActionResult Create()
+
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(Models.Market veri)
+        {
+            Models.Market.MarketListesi.Add(veri);
+            return RedirectToAction("Liste");
+
+        }
+
+            public ActionResult Edit(int Id)
+        {
+            var d1 = Models.Market.MarketListesi.Where(e => e.Id == Id).FirstOrDefault();
+            return View(d1);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Models.Market d2)
+        {
+            var d1 = Models.Market.MarketListesi.Where(e => e.Id == d2.Id).FirstOrDefault();
+            d1.Id = d2.Id;
+            d1.Ad = d2.Ad;
+            d1.Fiyat = d2.Fiyat;
+            return RedirectToAction("Liste");
+        }
+
+        public ActionResult Delete(int Id)
+
+        {
+
+            var g3 = Models.Market.MarketListesi.Where(x => x.Id == Id).FirstOrDefault();
+
+            return View(g3);
+
+        }
+
+        [HttpPost]
+        public ActionResult Delete(Models.Market h44)
+
+        {
+
+            var h4 = Models.Market.MarketListesi.Where(x => x.Id == h44.Id).FirstOrDefault();
+            Models.Market.MarketListesi.Remove(h4);
+            return RedirectToAction("Liste");
+
+        }
+
+    }
+}
---
+++ webVize/webVize/Models/Market.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace webVize.Models
+{
+    public class Market
+    {
+
+        public int Id { get; set; }
+        public string Ad { get; set; }
+        public string Fiyat { get; set; }
+
+
+        public static List<Market> MarketListesi = new List<Market>
+        {
+                 new Market
+             {
+                 Id=1,
+                 Ad="Bisküvi",
+                 Fiyat="2TL",
+             },
+                  new Market
+             {
+                 Id=2,
+                 Ad="Gofret",
+                 Fiyat="1TL",
+             },
+                   new Market
+             {
+                 Id=3,
+                 Ad="Kek",
+                 Fiyat="1TL",
+             }
+        };
+    }
+}
